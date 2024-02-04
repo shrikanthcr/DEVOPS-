718279103880a98cb52b11a5b4ac51df4f3d5e6e
shrikanthcr <107162817+shrikanthcr@users.noreply.github.com>
2024-02-04 14:32:45 +0530

    Create main.tf

---
+++ Terraform-Projects/ec2_tomcat/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "ec2terrafrom" {
+  ami           = "ami-0866a04d72a1f5479"
+  instance_type = "t2.micro"
+  key_name = "ohiokey"
+  user_data     = file("tomcat.sh")
+  security_groups = ["My_Security_Group1"]
+  count = 1
+  tags ={
+  Name = "my_terraform_tomcat"
+  }
+}
